d3feec9e664f9e23ccc522aa47692aa87d74d205
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2020-01-05 11:46:55 -0300

    Add user courses controller

---
+++ app/controllers/api/v1/user_courses_controller.rb
@@ -0,0 +1,52 @@
+module Api
+  module V1
+    class UserCoursesController < ApplicationController
+      before_action :authenticate_user!
+      before_action :set_user_course, only: [:show, :destroy, :update]
+
+      def index
+        @user_courses = current_user.user_courses
+        @user_courses = @user_courses
+          .where(semester_id: params[:semester_id]) unless params[:semester_id].nil?
+      end
+
+      def show
+      end
+
+      def create
+        @user_course = current_user.user_courses.create(user_course_params)
+        if @user_course.errors.blank?
+          render :show, status: :created
+        else
+          render json: @user_course.errors, status: :bad_request
+        end
+      end
+
+      def destroy
+        if @user_course.destroy
+          redirect_to action: :index
+        else
+          render status: :bad_request
+        end
+      end
+
+      def update
+        if @user_course.update(user_course_params)
+          render :show, status: :ok
+        else
+          render json: @user_course.errors, status: :bad_request
+        end
+      end
+
+      private
+
+      def user_course_params
+        params.require(:user_course).permit(:course_id, :semester_id, :status)
+      end
+
+      def set_user_course
+        @user_course = current_user.user_courses.find(params[:id])
+      end
+    end
+  end
+end
