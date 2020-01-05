136f0d41c133c69d158b27be4a2b749472d3c337
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2020-01-05 19:10:53 -0300

    Add courses controller (#6)

---
+++ app/controllers/api/v1/courses_controller.rb
@@ -0,0 +1,49 @@
+module Api
+  module V1
+    class CoursesController < ApplicationController
+      before_action :set_course, only: [:show, :destroy, :update]
+
+      def index
+        @courses = Course.by_institute(params[:institute_id])
+      end
+
+      def show
+      end
+
+      def create
+        @course = Course.create(course_params)
+        if @course.errors.blank?
+          render :show
+        else
+          render json: @course.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        if @course.destroy
+          redirect_to action: :index
+        else
+          render status: :bad_request
+        end
+      end
+
+      def update
+        if @course.update(course_params)
+          render :show
+        else
+          render json: @course.errors, status: :bad_request
+        end
+      end
+
+      private
+
+      def course_params
+        params.require(:course).permit(:name, :credits, :institute_id)
+      end
+
+      def set_course
+        @course = Course.find(params[:id])
+      end
+    end
+  end
+end
---
+++ app/views/api/v1/courses/show.json.jbuilder
@@ -0,0 +1,9 @@
+json.course do
+  json.id @course.id
+  json.name @course.name
+  json.credits @course.credits
+  json.institute do
+    json.id @course.institute.id
+    json.name @course.institute.name
+  end
+end
