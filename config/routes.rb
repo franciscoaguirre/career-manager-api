0cf85b037297fdfad7bed69e30541afb8f08f3bc
Santiago Calvo <sncalvo@hotmail.com>
2020-01-13 18:37:53 -0300

    Create exams instances controller (#14)
    * Add exams controller
    
    * Add user courses controller
    
    * Add courses controller (#6)
    
    * Add semesters controller (#7)
    
    * Add exam instances controller
    
    Co-authored-by: Francisco Aguirre <franciscoaguirreperez@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :exam_instances
       resources :semesters
       resources :courses
       resources :user_courses
