37d051c60436ffafa7449da8f53027463c072ed6
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2020-01-13 23:38:08 -0300

    Add request format code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   respond_to :json
+  before_action :set_default_request_format
+
+  protected
+
+  def set_default_request_format
+    request.format = :json unless params[:format]
+  end
 end
